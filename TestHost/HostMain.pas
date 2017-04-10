a7cf118f7a281ca860a1580b00ea951f45a62149
Mark <inpwtepydjuf@gmail.com>
2017-04-10 14:17:21 +0100

    Co-opt automation test project to prove it can work

--- TestHost/HostMain.pas
+++ TestHost/HostMain.pas
@@ -1,3 +1,24 @@
+{***************************************************************************}
+{                                                                           }
+{           DelphiUIAutomation                                              }
+{                                                                           }
+{           Copyright 2015 JHC Systems Limited                              }
+{                                                                           }
+{***************************************************************************}
+{                                                                           }
+{  Licensed under the Apache License, Version 2.0 (the "License");          }
+{  you may not use this file except in compliance with the License.         }
+{  You may obtain a copy of the License at                                  }
+{                                                                           }
+{      http://www.apache.org/licenses/LICENSE-2.0                           }
+{                                                                           }
+{  Unless required by applicable law or agreed to in writing, software      }
+{  distributed under the License is distributed on an "AS IS" BASIS,        }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
+{  See the License for the specific language governing permissions and      }
+{  limitations under the License.                                           }
+{                                                                           }
+{***************************************************************************}
 unit HostMain;
 
 interface
@@ -5,18 +26,78 @@ interface
 uses
   RestServer,
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.Menus,
+  AutomatedCombobox, AutomatedEdit, Vcl.Grids, AutomatedStringGrid, Vcl.Mask,
+  AutomatedMaskEdit, Vcl.ToolWin, Vcl.ExtCtrls, Vcl.ImgList, System.ImageList,
+  AutomatedStaticText, Vcl.Buttons;
 
 type
-  TForm2 = class(TForm)
+  TForm1 = class(TForm)
+    Edit2: TEdit;
+    Button1: TButton;
+    Button2: TButton;
     PageControl1: TPageControl;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
     TabSheet3: TTabSheet;
-    TabSheet4: TTabSheet;
-    TabSheet5: TTabSheet;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    RadioButton3: TRadioButton;
+    StatusBar1: TStatusBar;
+    ComboBox1: TComboBox;
+    MainMenu1: TMainMenu;
+    File1: TMenuItem;
+    Hel1: TMenuItem;
+    Exit1: TMenuItem;
+    About1: TMenuItem;
+    PopupMenu1: TPopupMenu;
+    PopupMenu2: TMenuItem;
+    AutomatedEdit1: TAutomatedEdit;
+    AutomatedCombobox1: TAutomatedCombobox;
+    AutomatedCombobox2: TAutomatedCombobox;
+    AutomationStringGrid1: TAutomationStringGrid;
+    AutomatedMaskEdit1: TAutomatedMaskEdit;
+    RichEdit1: TRichEdit;
+    TreeView1: TTreeView;
+    PopupMenu3: TPopupMenu;
+    Popup11: TMenuItem;
+    Popup21: TMenuItem;
+    Edit1: TEdit;
     ListBox1: TListBox;
+    LinkLabel1: TLinkLabel;
+    Panel6: TPanel;
+    Panel7: TPanel;
+    ToolBar1: TToolBar;
+    ToolButton3: TToolButton;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton4: TToolButton;
+    Panel8: TPanel;
+    ToolBar2: TToolBar;
+    ToolButton5: TToolButton;
+    ToolButton6: TToolButton;
+    ToolButton7: TToolButton;
+    ToolButton8: TToolButton;
+    ImageList1: TImageList;
+    AutomatedStaticText1: TAutomatedStaticText;
+    ListBox2: TListBox;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Exit1Click(Sender: TObject);
+    procedure PopupMenu2Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure ToolButton1Click(Sender: TObject);
+    procedure ToolButton5Click(Sender: TObject);
+    procedure ToolButton7Click(Sender: TObject);
+    procedure LinkLabel1Click(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
     { Private declarations }
@@ -29,37 +110,96 @@ type
   end;
 
 var
-  Form2: TForm2;
+  Form1: TForm1;
 
 implementation
 
 {$R *.dfm}
 
-procedure TForm2.FormCreate(Sender: TObject);
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage (edit1.Text + ' | ' + edit2.Text);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  ShowMessage ('Cancelled');
+end;
+
+procedure TForm1.Exit1Click(Sender: TObject);
+begin
+  ShowMessage('Oh well done');
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
 begin
   createServer(4723);
+
+  AutomationStringGrid1.Cells[0,0] := 'Title 1';
+  AutomationStringGrid1.Cells[1,0] := 'Title 2';
+  AutomationStringGrid1.Cells[2,0] := 'Title 3';
+  AutomationStringGrid1.Cells[3,0] := 'Title 4';
+  AutomationStringGrid1.Cells[4,0] := 'Title 5';
+
+  AutomationStringGrid1.Cells[0,1] := 'Row 1, Col 0';
+  AutomationStringGrid1.Cells[1,1] := 'Row 1, Col 1';
+  AutomationStringGrid1.Cells[2,1] := 'Row 1, Col 2';
+  AutomationStringGrid1.Cells[3,1] := 'Row 1, Col 3';
+  AutomationStringGrid1.Cells[4,1] := 'Row 1, Col 4';
+
+  AutomationStringGrid1.Cells[0,3] := 'Row 3, Col 0';
+  AutomationStringGrid1.Cells[1,3] := 'Row 3, Col 1';
+  AutomationStringGrid1.Cells[2,3] := 'Row 3, Col 2';
+  AutomationStringGrid1.Cells[3,3] := 'Row 3, Col 3';
+  AutomationStringGrid1.Cells[4,3] := 'Row 3, Col 4';
+
 end;
 
-procedure TForm2.FormDestroy(Sender: TObject);
+procedure TForm1.FormDestroy(Sender: TObject);
 begin
-  destroyServer;
+  FRestServer.Free;
 end;
 
-procedure TForm2.CreateServer(port: word);
+procedure TForm1.LinkLabel1Click(Sender: TObject);
+begin
+  ShowMessage ('LinkLabel1Click');
+end;
+
+procedure TForm1.PopupMenu2Click(Sender: TObject);
+begin
+  ShowMessage ('Popup menu clicked');
+end;
+
+procedure TForm1.ToolButton1Click(Sender: TObject);
+begin
+  ShowMessage ('ToolButton1Click');
+end;
+
+procedure TForm1.ToolButton5Click(Sender: TObject);
+begin
+  ShowMessage ('ToolButton5Click');
+end;
+
+procedure TForm1.ToolButton7Click(Sender: TObject);
+begin
+  ShowMessage ('ToolButton7Click');
+end;
+
+procedure TForm1.CreateServer(port: word);
 begin
   FRestServer := TRestServer.Create(nil);
   FRestServer.OnLogMessage := LogMessage;
   FRestServer.Start(port);
 end;
 
-procedure TForm2.DestroyServer;
+procedure TForm1.DestroyServer;
 begin
   FRestServer.Free;
 end;
 
-procedure TForm2.LogMessage(const msg: String);
+procedure TForm1.LogMessage(const msg: String);
 begin
-  listbox1.Items.Add(msg);
+  ListBox2.Items.Add(msg);
 end;
 
 end.
--- TestHost/TestHost.dpr
+++ TestHost/TestHost.dpr
@@ -1,14 +1,42 @@
+{***************************************************************************}
+{                                                                           }
+{           DelphiUIAutomation                                              }
+{                                                                           }
+{           Copyright 2015-16 JHC Systems Limited                              }
+{                                                                           }
+{***************************************************************************}
+{                                                                           }
+{  Licensed under the Apache License, Version 2.0 (the "License");          }
+{  you may not use this file except in compliance with the License.         }
+{  You may obtain a copy of the License at                                  }
+{                                                                           }
+{      http://www.apache.org/licenses/LICENSE-2.0                           }
+{                                                                           }
+{  Unless required by applicable law or agreed to in writing, software      }
+{  distributed under the License is distributed on an "AS IS" BASIS,        }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
+{  See the License for the specific language governing permissions and      }
+{  limitations under the License.                                           }
+{                                                                           }
+{***************************************************************************}
 program TestHost;
 
 uses
   Vcl.Forms,
-  HostMain in 'HostMain.pas' {Form2};
+  HostMain in 'HostMain.pas' {Form1},
+  AutomatedCombobox in '..\..\DelphiUIAutomation\controls\AutomatedCombobox.pas',
+  UIAutomationCore_TLB in '..\..\DelphiUIAutomation\controls\UIAutomationCore_TLB.pas',
+  AutomatedStringGrid in '..\..\DelphiUIAutomation\controls\AutomatedStringGrid.pas',
+  StringGridItem in '..\..\DelphiUIAutomation\controls\StringGridItem.pas',
+  AutomatedEdit in '..\..\DelphiUIAutomation\controls\AutomatedEdit.pas',
+  AutomatedMaskEdit in '..\..\DelphiUIAutomation\controls\AutomatedMaskEdit.pas',
+  AutomatedStaticText in '..\..\DelphiUIAutomation\controls\AutomatedStaticText.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
